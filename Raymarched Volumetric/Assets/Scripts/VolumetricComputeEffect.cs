5d8bce8334a1a60fab1d6385fc2b3be80bd04a2a
Vladyslav Michurin <goduster.mail@gmail.com>
2026-03-30 06:50:34 -0400

    downsampling itterations

--- Raymarched Volumetric/Assets/Scripts/VolumetricComputeEffect.cs
+++ Raymarched Volumetric/Assets/Scripts/VolumetricComputeEffect.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using CameraCommon;
+
 [RequireComponent(typeof(Camera))]
 [ExecuteInEditMode]
 public class VolumetricComputeEffect : MonoBehaviour
 {
     public Light sun;
 
-    public Color extinctionColor = new Color(1, 0, 0, 1);
-    public Color scateringColor = new Color(1, 1, 1, 1);
-
-    public Vector3 spherePosition = new Vector3(0, 0, - 10);
+    public Vector3 spherePosition = new Vector3(0, 0, -7);
     [Min(0.1f)]
     public float sphereRadius = 3f;
+    [Range(0, 5f)]
+    public int downsamplingItterations = 0;
     [Range(0f, 1f)]
     public float densityFalloff = 0.8f;
 
@@ -33,11 +34,17 @@ public class VolumetricComputeEffect : MonoBehaviour
     [Min(0f)]
     public float volumeDensity = 1f;
     [Range(-1f, 1f)]
-    public float asymmetryFactor = -0.35f;
+    public float asymmetryFactor = 0;
 
     [SerializeField]
     protected ComputeShader volumetricCompute;
-    private RenderTexture computeOutput = null;
+    private RenderTexture computeColor = null;
+    private RenderTexture computeTransmittance = null;
+    private RenderTexture computeColor_FullRez = null;
+    private RenderTexture computeTransmittance_FullRez = null;
+    [SerializeField]
+    protected Shader volumetricCompositeShader;
+    private Material volumetricCompositeMaterial;
 
     private void OnRenderImage(RenderTexture _source, RenderTexture _destination)
     {
@@ -46,7 +53,12 @@ public class VolumetricComputeEffect : MonoBehaviour
             Graphics.Blit(_source, _destination);
             return;
         }
-        if(!sun)
+        if (!BB_Rendering.ShaderMaterialReady(volumetricCompositeShader, ref volumetricCompositeMaterial))
+        {
+            Graphics.Blit(_source, _destination);
+            return;
+        }
+        if (!sun)
         {
             Graphics.Blit(_source, _destination);
             return;
@@ -55,52 +67,67 @@ public class VolumetricComputeEffect : MonoBehaviour
         ////////////////////////////
         int width = _source.width;
         int height = _source.height;
+        CreateTempRT(ref computeColor_FullRez, width, height);
+        CreateTempRT(ref computeTransmittance_FullRez, width, height);
 
-        if (computeOutput == null)
+        int computeWidth = width;
+        int computeHeight = height;
+        for(int i = 0; i < downsamplingItterations; i++)
         {
-            CreateRenderTexture(ref computeOutput, width, height);
+            computeWidth /= 2;
+            computeHeight /= 2;
         }
+        CreateTempRT(ref computeColor, computeWidth, computeHeight);
+        CreateTempRT(ref computeTransmittance, computeWidth, computeHeight);
 
-        DispatchComputeShader(width, height);
+        DispatchComputeShader(computeWidth, computeHeight);
 
-        Graphics.Blit(computeOutput, _destination);
+        Graphics.Blit(computeColor, computeColor_FullRez);
+        Graphics.Blit(computeTransmittance, computeTransmittance_FullRez);
+
+        volumetricCompositeMaterial.SetTexture("_VolumetricColor", computeColor_FullRez);
+        volumetricCompositeMaterial.SetTexture("_VolumetricTransmittance", computeTransmittance_FullRez);
+
+        Graphics.Blit(_source, _destination, volumetricCompositeMaterial);
     }
 
-    void CreateRenderTexture(ref RenderTexture _renderTexture, int _width, int _height)
+    void CreateTempRT(ref RenderTexture _renderTexture, int _width, int _height)
     {
         if (_renderTexture != null)
         {
-            _renderTexture.Release();
-            _renderTexture = null;
+            RenderTexture.ReleaseTemporary(_renderTexture);
         }
 
-        _renderTexture = new RenderTexture(_width, _height, 0, RenderTextureFormat.ARGBFloat);
+        _renderTexture = RenderTexture.GetTemporary(_width, _height, 0, RenderTextureFormat.ARGBFloat);
         _renderTexture.enableRandomWrite = true;
-
-        _renderTexture.Create();
     }
     void DispatchComputeShader(int _width, int _height)
     {
         int kernelIndex = volumetricCompute.FindKernel("CS_RaymarchVolumetric");
 
-        volumetricCompute.SetTexture(kernelIndex, "Result", computeOutput);
-        volumetricCompute.SetVector("_SpherePosition", spherePosition);
-        volumetricCompute.SetFloat("_SphereRadius", sphereRadius);
+        volumetricCompute.SetTexture(kernelIndex, "VolumetricColor", computeColor);
+        volumetricCompute.SetTexture(kernelIndex, "VolumetricTransmittance", computeTransmittance);
+
         volumetricCompute.SetVector("_ScreenResolution", new Vector2(_width, _height));
-        volumetricCompute.SetTexture(kernelIndex, "_CameraDepthTexture", Shader.GetGlobalTexture("_CameraDepthTexture"));
+        volumetricCompute.SetVector("_WorldSpaceCameraPos", Camera.main.transform.position);
         Matrix4x4 projMatrix = GL.GetGPUProjectionMatrix(Camera.main.projectionMatrix, false);
         Matrix4x4 viewProjMatrix = projMatrix * Camera.main.worldToCameraMatrix;
         volumetricCompute.SetMatrix("_CameraProjectionToWorld", Matrix4x4.Inverse(viewProjMatrix));
-        volumetricCompute.SetVector("_WorldSpaceCameraPos", Camera.main.transform.position);
+        volumetricCompute.SetTexture(kernelIndex, "_CameraDepthTexture", Shader.GetGlobalTexture("_CameraDepthTexture"));
+
+        volumetricCompute.SetVector("_SunDirection", sun.transform.forward);
+        volumetricCompute.SetVector("_SunColor", sun.color);
+
+        volumetricCompute.SetVector("_SpherePosition", spherePosition);
+        volumetricCompute.SetFloat("_SphereRadius", sphereRadius);
+
+        volumetricCompute.SetFloat("_DensityFalloff", densityFalloff);
+        volumetricCompute.SetFloat("_StepSize", stepSize);
+        volumetricCompute.SetFloat("_LightStepSize", lightStepSize);
         volumetricCompute.SetVector("_AbsorptionCoef", absorptionCoef * absorptionMultipier);
         volumetricCompute.SetVector("_ScatteringCoef", scatteringCoef * scatteringMultipier);
         volumetricCompute.SetFloat("_VolumeDensity", volumeDensity);
-        volumetricCompute.SetFloat("_StepSize", stepSize);
-        volumetricCompute.SetFloat("_LightStepSize", lightStepSize);
-        volumetricCompute.SetFloat("_DensityFalloff", densityFalloff);
         volumetricCompute.SetFloat("_AsymmetryFactor", asymmetryFactor);
-        volumetricCompute.SetVector("_SunDirection", sun.transform.forward);
-        volumetricCompute.SetVector("_SunColor", sun.color);
 
         int threadsCountX = Mathf.CeilToInt(_width / 8f);
         int threadsCountY = Mathf.CeilToInt(_height / 8f);
--- Raymarched Volumetric/Assets/Shaders/VolumetricComposite.shader
+++ Raymarched Volumetric/Assets/Shaders/VolumetricComposite.shader
@@ -1,4 +1,4 @@
-Shader "Unlit/VolumetricComposite"
+Shader "_BB/VolumetricComposite"
 {
     Properties
     {
@@ -6,16 +6,15 @@ Shader "Unlit/VolumetricComposite"
     }
     SubShader
     {
-        Tags { "RenderType"="Opaque" }
-        LOD 100
+        Cull Off
+        ZWrite Off
+        ZTest Always
 
         Pass
         {
             CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
-            // make fog work
-            #pragma multi_compile_fog
 
             #include "UnityCG.cginc"
 
@@ -28,29 +27,31 @@ Shader "Unlit/VolumetricComposite"
             struct v2f
             {
                 float2 uv : TEXCOORD0;
-                UNITY_FOG_COORDS(1)
                 float4 vertex : SV_POSITION;
             };
 
             sampler2D _MainTex;
-            float4 _MainTex_ST;
+            sampler2D _VolumetricColor, _VolumetricTransmittance;
+            
 
             v2f vert (appdata v)
             {
                 v2f o;
+
                 o.vertex = UnityObjectToClipPos(v.vertex);
-                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
-                UNITY_TRANSFER_FOG(o,o.vertex);
+                o.uv = v.uv;
+ 
                 return o;
             }
 
-            fixed4 frag (v2f i) : SV_Target
+            float4 frag (v2f i) : SV_Target
             {
-                // sample the texture
-                fixed4 col = tex2D(_MainTex, i.uv);
-                // apply fog
-                UNITY_APPLY_FOG(i.fogCoord, col);
-                return col;
+                float4 src = tex2D(_MainTex, i.uv);
+                float3 volumetricColor = tex2D(_VolumetricColor, i.uv);
+                float3 transmittance = tex2D(_VolumetricTransmittance, i.uv);
+
+                float3 finalOutput = src * transmittance + volumetricColor;
+                return float4(finalOutput, 1);
             }
             ENDCG
         }
